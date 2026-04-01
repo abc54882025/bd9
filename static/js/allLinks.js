9ec4b455b03c8fa8eb1ec214c20554b44ede5885
abc54882025 <abc54882025@gmail.com>
2026-04-01 12:24:57 +0800

    Add files via upload

---
+++ static/js/allLinks.js
@@ -0,0 +1,12 @@
+    const allLinks = [
+                    "https://www.hi77.mobi",
+                    "https://www.Hi77.xyz",
+                    "https://www.hi77.mobi",
+                    "https://www.Hi77.xyz",
+                    "https://www.hi77.mobi",
+                    "https://www.hi77.mobi",
+                    "https://www.Hi77.xyz",
+                    "https://www.hi77.mobi",
+                    "https://www.Hi77.xyz",
+                    "https://www.hi77.mobi", 
+        ];
